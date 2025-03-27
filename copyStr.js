e99e498ea4d740656360e7d2a10fc40b564cd0c0
xiaolidan00 <764937567@qq.com>
2025-03-27 11:26:37 +0800

    feat: 复制文本，uuid

---
+++ copyStr.js
@@ -0,0 +1,10 @@
+export const copyStr = (str: string) => {
+  const dom = document.createElement('input');
+  dom.value = str;
+  dom.style.position = 'fixed';
+  dom.style.opacity = '0';
+  document.body.appendChild(dom);
+  dom.select();
+  document.execCommand('copy');
+  document.body.removeChild(dom);
+};
