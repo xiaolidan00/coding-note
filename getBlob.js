1d17a90d578ca48ddebc7b297ce5888639e192c6
xiaolidan00 <764937567@qq.com>
2025-06-27 15:57:33 +0800

    fix: 下载blob

--- getBlob.js
+++ getBlob.js
@@ -1,8 +1,8 @@
 export function getBlob(url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
     xhr.onload = function () {
       if (xhr.status === 200) {
         resolve(xhr.response);
@@ -13,5 +13,22 @@ export function getBlob(url) {
       reject();
     };
     xhr.send();
+  });
+}
+
+export function getBlob1() {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Cache-Control": "no-cache"
+      }
+    })
+      .then((res) => {
+        resolve(res.blob());
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 }
